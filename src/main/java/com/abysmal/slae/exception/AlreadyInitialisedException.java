ebade3ba24a8e1a0c77580ad08e1ed69f4a98d1b
Mojken <mikaelgillis@gmail.com>
2018-02-04 23:19:33 +0100

    Format

--- src/main/java/com/abysmal/slae/exception/AlreadyInitialisedException.java
+++ src/main/java/com/abysmal/slae/exception/AlreadyInitialisedException.java
@@ -4,7 +4,7 @@ public class AlreadyInitialisedException extends RuntimeException {
 
 	private static final long serialVersionUID = 1L;
 
-	public AlreadyInitialisedException(String msg){
+	public AlreadyInitialisedException(String msg) {
 		super(msg);
 	}
 
